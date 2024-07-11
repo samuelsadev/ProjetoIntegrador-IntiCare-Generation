f18c5384a421ec2f69d54124d515ed4e1575c276
samuelsadev <samuelsa.dev@gmail.com>
2024-07-11 11:56:07 -0300

    Criação do CRUD

--- src/main/java/com/generation/inticare/repository/CategoriaRepository.java
+++ src/main/java/com/generation/inticare/repository/CategoriaRepository.java
@@ -2,8 +2,13 @@ package com.generation.inticare.repository;
 
 import com.generation.inticare.model.CategoriaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+    List<CategoriaModel> findAllByGeneroContainingIgnoreCase(@Param("genero")String genero);
+    List<CategoriaModel> findAllByNomeContainingIgnoreCase(@Param("nome")String nome);
 }
