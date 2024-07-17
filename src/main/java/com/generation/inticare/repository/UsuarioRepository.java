8adef2ee90fa3b3dcf7c7b489748c5fa47f2a1ab
samuelsadev <samuelsa.dev@gmail.com>
2024-07-17 14:10:59 -0300

    Criando classe UsuarioLogin e adicionando método a interface UsuarioRepository

--- src/main/java/com/generation/inticare/repository/UsuarioRepository.java
+++ src/main/java/com/generation/inticare/repository/UsuarioRepository.java
@@ -4,6 +4,9 @@ import com.generation.inticare.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+    Optional<UsuarioModel> findByUsuario(String usuario);
 }
